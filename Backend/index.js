86a099f06af6c5a23fb0bf0eee1acf4d15188ea7
Enzo Hugonnier <hugonnierenzo@gmail.com>
2023-11-21 21:35:35 +0100

    Add dabatase logon

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,23 @@
 // Express package
 const express = require("express");
-
+// Mysql package
+const mysql = require("mysql");
+// dotenv package
+const { config } = require('dotenv');
 
 // App settings
 const app = express();
-const PORT = 3333;
+app.use(express.json());
+config(); // setup dotenv
+const PORT = 3333; // Default port used for the server
+
+// Database settings
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
 
 // App bootup
 app.listen(PORT, () => {
