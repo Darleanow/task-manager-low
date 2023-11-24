85d61fe586f7baf3b9376b256ba0cb3759e11fdf
Jaden38 <damien.nithard@gmail.com>
2023-11-24 17:48:58 +0100

    Home Page No Projects

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <div className="flex-container header"></div>
+      <div className="flex-container body">
+        <div className="body-header">
+          <div className="all-projects-title">All Projects</div>
+          <div className="new-project-button">New Project</div>
+        </div>
+        <div className="body-content">
+          <div className="content-left">
+            <img src="/images/3d-boy.png" alt="3D Boy" />
+          </div>
+          <div className="content-right">
+            <p>No projects</p>
+            <p> yet.</p>
+            <p> Create one !</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex-container footer">
+        All rights reserved to Damien & Enzo. 2023.
+      </div>
     </div>
   );
 }
