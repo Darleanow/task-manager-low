80de0950c93ca35d98af49495d9e530b0151185a
Enzo Hugonnier <hugonnierenzo@gmail.com>
2023-12-02 00:07:00 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import "./App.scss";
 import ProjectPage from "./ProjectPage/ProjectPage";
+
+const noProjects = process.env.PUBLIC_URL + "/images/NoProject.svg";
+
+const projects = [
+  {
+    name: "Project 1",
+    description:
+      "Here’s a sample description that gets cut if it’s too long, but it needs to be really long",
+    issue_count: 40,
+    isFav: true,
+  },
+
+  {
+    name: "Bootstrap",
+    description:
+      "Bootstrap is a project that brings the css wrapper already huge, to another leve...",
+    issue_count: 0,
+    isFav: false,
+  },
+
+  {
+    name: "Bootstrap 2",
+    description:
+      "Bootstrap is a project that brings the css wrapper already huge, to another leve...",
+    issue_count: 0,
+    isFav: false,
+  }
+];
+
 const App = () => {
-  const hasProjects = false;
+  const hasProjects = true;
 
   return (
     <div className="a-main_window">
@@ -40,16 +69,26 @@ const App = () => {
         </div>
       </div>
       <div className="a-main_content">
-        {" "}
         {hasProjects ? (
-          /**Map projects */
-          <>
-            <p> Hello projects</p>
-            <button> test </button>
-          </>
+          <div className="a-project_list">
+            {projects.map((project) => (
+              <div key={project.id}>
+                <div className="a-project_child">
+                  <p>{project.name}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         ) : (
-          <div className="a-empty_projects"></div>
-        )}{" "}
+          <div className="a-empty_projects">
+            <div className="a-svg">
+              <img src={noProjects} alt="Empty Projects" />
+            </div>
+            <div className="a-empty_text">
+              There are no projects created yet
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
