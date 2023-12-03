1f50017dfa808bfb57fcceca462beb5dda3d1e2b
Enzo Hugonnier <hugonnierenzo@gmail.com>
2023-12-03 19:36:41 +0100

    Secure routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./Utils/Routing/ProtectedRoute";
 import ProjectPage from "./ProjectPage/ProjectPage";
 import TasksPage from "./TasksPage/TasksPage";
 import AuthentPage from "./AuthentPage/AuthentPage";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<AuthentPage />} />
-        <Route path="projects" element={<ProjectPage />} />
-        <Route path="projects/dashboard" element={<TasksPage />} />
+        <Route path="/projects" element={<ProtectedRoute />}>
+          <Route index element={<ProjectPage />} />
+          <Route path=":projectId/tasks" element={<TasksPage />} />
+        </Route>
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
 
