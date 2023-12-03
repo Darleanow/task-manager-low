a576836122269aa90e37bdf475735b310a3dd0de
Enzo Hugonnier <hugonnierenzo@gmail.com>
2023-12-03 11:45:02 +0100

    enhance logic and ux, WIP for routing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ const App = () => {
   const [password, setPassword] = useState("");
   const [fullName, setFullName] = useState("");
 
+  const [userData, setUserData] = useState(null);
+
   useEffect(() => {
+    if (isLogged || checkTokenValidity()) {
+      fetchUserData();
+    }
+
     if (!checkTokenValidity()) {
       setIsLogged(false);
     }
@@ -60,18 +66,38 @@ const App = () => {
     setIsLogged(false);
     window.location.reload();
   };
-  
 
   const checkTokenValidity = () => {
     const tokenExpiry = localStorage.getItem("tokenExpiry");
     return tokenExpiry && Date.now() <= parseInt(tokenExpiry);
   };
 
+  const fetchUserData = async () => {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await fetch("http://localhost:3333/user", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setUserData(data);
+      } else {
+        console.error("Error fetching user data");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   // Render logic
   if (isLogged || checkTokenValidity()) {
     return (
       <div className="a-main_content">
-        Hello
+        Hello, {userData ? userData.full_name : "User"}
         <button onClick={handleLogout}>Logout</button>
       </div>
     );
@@ -121,6 +147,13 @@ const App = () => {
           />
           <button type="submit">Register</button>
         </form>
+        <button
+          onClick={() => {
+            setAlreadyRegistered(true);
+          }}
+        >
+          Already registered ?
+        </button>
       </div>
     );
   }
