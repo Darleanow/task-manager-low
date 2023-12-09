35c880b10808c368c0f5098866bd23993feea0d0
Enzo Hugonnier <hugonnierenzo@gmail.com>
2023-12-09 19:35:48 +0100

    notifications queries

--- Backend/models/userModels.js
+++ Backend/models/userModels.js
@@ -16,21 +16,27 @@ async function registerUser(full_name, email, password) {
 }
 
 async function loginUser(email, password) {
-  const query = "SELECT user_id, full_name, user_role, user_picture, email FROM Users WHERE email = ?";
+  const query =
+    "SELECT user_id, full_name, user_role, user_picture, email, password FROM Users WHERE email = ?";
   return new Promise((resolve, reject) => {
     db.query(query, [email], async (err, results) => {
       if (err) reject(err);
       if (results.length > 0) {
-        const validPassword = await bcrypt.compare(password, results[0].password);
+        const validPassword = await bcrypt.compare(
+          password,
+          results[0].password
+        );
         if (validPassword) {
-          const user = { 
-            user_id: results[0].user_id, 
-            full_name: results[0].full_name, 
-            user_role: results[0].user_role, 
-            user_picture: results[0].user_picture, 
-            email: results[0].email 
+          const user = {
+            user_id: results[0].user_id,
+            full_name: results[0].full_name,
+            user_role: results[0].user_role,
+            user_picture: results[0].user_picture,
+            email: results[0].email,
           };
-          const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "24h" });
+          const token = jwt.sign(user, process.env.JWT_SECRET, {
+            expiresIn: "24h",
+          });
           resolve(token);
         } else {
           resolve(null);
@@ -56,4 +62,74 @@ async function getUserById(userId) {
   });
 }
 
-module.exports = { registerUser, loginUser, getUserById };
+async function getAllusersButID(userId) {
+  const query =
+    "SELECT full_name, email, user_role, user_picture FROM Users WHERE user_id != ?";
+  return new Promise((resolve, reject) => {
+    db.query(query, [userId], (err, results) => {
+      if (err) reject(err);
+      if (results.length > 0) {
+        resolve(results);
+      } else {
+        resolve(null);
+      }
+    });
+  });
+}
+
+async function getUserNotifications(userId) {
+  const query =
+    "SELECT * FROM Notifications WHERE user_id = ? ORDER BY creation_date DESC;";
+
+  return new Promise((resolve, reject) => {
+    db.query(query, [userId], (err, results) => {
+      if (err) reject(err);
+      if (results.length > 0) {
+        resolve(results);
+      } else {
+        resolve(null);
+      }
+    });
+  });
+}
+
+async function setNotificationRead(userId, notificationId) {
+  const query =
+    "update notifications set is_read = true where notification_id = ? and user_id = ?;";
+
+  return new Promise((resolve, reject) => {
+    db.query(query, [notificationId, userId], (err, results) => {
+      if (err) reject(err);
+      if (results.length > 0) {
+        resolve(results[0]);
+      } else {
+        resolve("Worked anyways");
+      }
+    });
+  });
+}
+
+async function setAllNotificationRead(userId) {
+  const query = "update notifications set is_read = true where user_id = ?;";
+
+  return new Promise((resolve, reject) => {
+    db.query(query, [userId], (err, results) => {
+      if (err) reject(err);
+      if (results.length > 0) {
+        resolve(results[0]);
+      } else {
+        resolve(null);
+      }
+    });
+  });
+}
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getUserById,
+  getAllusersButID,
+  getUserNotifications,
+  setNotificationRead,
+  setAllNotificationRead
+};
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -5,45 +5,104 @@ const router = express.Router();
 
 // Register endpoint
 router.post("/register", async (req, res) => {
-    const { full_name, email, password } = req.body;
-    try {
-        const result = await userModel.registerUser(full_name, email, password);
-        res.status(201).send(result);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Server error");
-    }
+  const { full_name, email, password } = req.body;
+  try {
+    const result = await userModel.registerUser(full_name, email, password);
+    res.status(201).send(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
 });
 
 // Login endpoint
 router.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const token = await userModel.loginUser(email, password);
-        if (token) {
-            res.status(200).json({ token });
-        } else {
-            res.status(401).send("Invalid credentials");
-        }
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Error during login");
+  const { email, password } = req.body;
+  try {
+    const token = await userModel.loginUser(email, password);
+    if (token) {
+      res.status(200).json({ token });
+    } else {
+      res.status(401).send("Invalid credentials");
     }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error during login");
+  }
 });
 
 // Get user endpoint
 router.get("/get_user", authenticateToken, async (req, res) => {
-    try {
-        const userData = await userModel.getUserById(req.user.user_id);
-        if (userData) {
-            res.status(200).json(userData);
-        } else {
-            res.status(404).send("User not found");
-        }
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Error fetching user data");
+  try {
+    const userData = await userModel.getUserById(req.user.user_id);
+    if (userData) {
+      res.status(200).json(userData);
+    } else {
+      res.status(404).send("User not found");
     }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching user data");
+  }
 });
+
+router.post("/get_all_users", authenticateToken, async (req, res) => {
+  const { user_id } = req.body;
+  try {
+    const userData = await userModel.getAllusersButID(user_id);
+    if (userData) {
+      res.status(200).json(userData);
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching user data");
+  }
+});
+
+router.post("/get_notifications", authenticateToken, async (req, res) => {
+  const { user_id } = req.body;
+  try {
+    const notifications = await userModel.getUserNotifications(user_id);
+    if (notifications) {
+      res.status(200).json(notifications);
+    } else {
+      res.status(200).send("No notifications");
+    }
+  } catch (err) {
+    res.status(500).send("Error fetching notifications.");
+  }
+});
+
+router.post("/read_notification", authenticateToken, async (req, res) => {
+  const { user_id, notification_id } = req.body;
+  try {
+    const response = await userModel.setNotificationRead(user_id, notification_id);
+    if (response) {
+      res.status(200).json(response);
+    } else {
+      res.status(404).send("huh?");
+    }
+  } catch (err) {
+    res.status(500).send("Error reading notifications (xD).");
+  }
+});
+
+router.post("/read_all_notifications", authenticateToken, async (req, res) => {
+  const { user_id } = req.body;
+  try {
+    const response = await userModel.setAllNotificationRead(user_id);
+    if (response) {
+      res.status(200).json(response);
+    } else {
+      res.status(200).send("No notifications");
+    }
+  } catch (err) {
+    res.status(500).send("Error fetching notifications.");
+  }
+});
+
+
 
 module.exports = router;
