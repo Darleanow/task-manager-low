b5f75320d5a286d10a176619c2e7dad02018dc45
Enzo Hugonnier <hugonnierenzo@gmail.com>
2023-12-13 21:43:20 +0100

    mapping from project page to taskpage

--- src/TasksPage/TasksPage.js
+++ src/TasksPage/TasksPage.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router";
+
+import { useNavigate } from "react-router-dom";
+
+import { useDispatch, useSelector } from "react-redux";
+
+
+import { jwtDecode } from "jwt-decode";
+import { checkTokenValidity } from "../Utils/BulkUtilsImport";
+import { login } from "../Utils/Routing/store";
+
 import PrimaryMenuBar from "../Components/PrimaryMenuBar/PrimaryMenuBar";
 import SecondaryMenuBar from "../Components/SecondaryMenuBar/SecondaryMenuBar";
 import BoardList from "../Components/BoardList/BoardList";
 import "./TasksPage.scss";
 
 const TasksPage = () => {
+  const userId = useSelector((state) => state.auth.user.user_id);
   const { projectId } = useParams();
+  
+
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token && checkTokenValidity()) {
+      const userDetails = jwtDecode(token);
+      dispatch(login(userDetails));
+    }
+  })
+
   return (
     <>
       <div className="pp-menu_holder">
@@ -19,7 +44,7 @@ const TasksPage = () => {
       <div className="pp-main_content">
         <BoardList p_BoolSetCards={true} />
         <BoardList p_BoolSetCards={false} />
-        {/* <BoardList p_BoolSetCards={false} /> */}
+        <BoardList p_BoolSetCards={false} />
       </div>
     </>
   );
